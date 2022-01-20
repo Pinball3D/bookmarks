af09d27548ae4d5bbf4699d491d8f234122aecba
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-20 13:04:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ app.get('/main.css', (req, res) => {
 app.get('/index.js', (req, res) => {
   res.sendFile(process.cwd() + "/js/index.js");
 });
+app.get('/lol', (req, res)=> {
+res.writeHead(301, {
+      location: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    });
+res.end()
+});
 app.get('*', (req, res) => {
   res.sendFile(process.cwd() + "/html/404.html");
-})
+});
 //Start server
 app.listen(port, (req, res) => {
 console.log(`server listening on port: ${port}`)
