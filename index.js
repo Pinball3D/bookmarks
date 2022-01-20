0ee8e6ae8c3345bbd8490f5f2c2aceeadb448152
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-20 12:44:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 //Route setup
 app.get('/', (req, res) => {
-  
-  res.send('root route');
+  res.sendFile(process.cwd() + "/html/index.html");
+  console.log(req.query.debug);
+});
+app.get('/main.css', (req, res) => {
+  res.sendFile(process.cwd() + "/css/main.css");
+});
+app.get('/index.js', (req, res) => {
+  res.sendFile(process.cwd() + "/js/index.js");
+});
+app.get('*', (req, res) => {
+  res.sendFile(process.cwd() + "/html/404.html");
 })
 //Start server
 app.listen(port, (req, res) => {
