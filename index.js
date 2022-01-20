d7663492350f4a207807c72b0adb84e8c74518ad
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2022-01-20 12:45:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,191 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-//Route setup
-app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + "/html/index.html");
-  console.log(req.query.debug);
+//Bookmark Generation
+$.getJSON('https://pinball3d.github.io/storage/bookmark-code/registry.json', function(data){
+  console.log(data);
+  var code = "";
+  var i = 0;
+  console.log(data.length, i < data.length)
+  while (i < data.length) {
+    var url = "https://pinball3d.github.io/storage/bookmark-code/"+data[i].code;
+    console.log(url)
+    var a = document.createElement("a");
+    var base = "var script = document.createElement('script'); script.src = '"+url+"'; document.body.appendChild(script)";
+    console.log(base)
+    var code = "javascript: "+encodeURIComponent("(function(){" + base.trim() +  "})();");
+    a.href = code;
+    a.innerHTML = data[i].name;
+    a.id="on"
+document.getElementById(data[i].catg).appendChild(a);
+    i++;
+  }
 });
-app.get('/main.css', (req, res) => {
-  res.sendFile(process.cwd() + "/css/main.css");
+
+
+
+
+//Switch Tabs
+var color = "#292828";
+var selcolor = "#4a4646";
+var selected = "";
+var m = document.getElementById("main");
+var main = document.getElementById("content");
+var back = document.getElementById("backgrounds");
+var cursors = document.getElementById("cursors");
+var sounds = document.getElementById("sounds");
+var misc = document.getElementById("misc");
+var msg = document.getElementById("msg");
+var iconhouse = document.getElementById("house");
+var iconback = document.getElementById("bg");
+var iconcursor = document.getElementById("cur");
+var iconsound = document.getElementById("soun");
+var iconmisc = document.getElementById("mis");
+var message = "Welcome to the new bookmarks page!";
+
+
+function home() {
+  selected = "#house";
+  iconhouse.style.backgroundColor=selcolor;
+  iconback.style.backgroundColor=color;
+  iconcursor.style.backgroundColor=color;
+  iconsound.style.backgroundColor=color;
+  iconmisc.style.backgroundColor=color;
+  msg.textContent = message;
+  main.hidden = false;
+  back.hidden = true;
+  cursors.hidden = true;
+  sounds.hidden = true;
+  misc.hidden= true;
+}
+function background() {
+  selected="#bg";
+  iconback.style.backgroundColor=selcolor;
+  iconhouse.style.backgroundColor=color;
+  iconcursor.style.backgroundColor=color;
+  iconsound.style.backgroundColor=color;
+  iconmisc.style.backgroundColor=color;
+  main.hidden = true;
+  msg.textContent = "Backgrounds";
+  back.hidden = false;
+  cursors.hidden = true;
+  sounds.hidden = true;
+  misc.hidden= true;
+}
+function cursor() {
+  selected="#cur";
+  iconcursor.style.backgroundColor=selcolor;
+  iconback.style.backgroundColor=color;
+  iconhouse.style.backgroundColor=color;
+  iconsound.style.backgroundColor=color;
+  iconmisc.style.backgroundColor=color;
+  main.hidden = true;
+  msg.textContent = "Cursors";
+  back.hidden = true;
+  cursors.hidden = false;
+  sounds.hidden = true;
+  misc.hidden= true;
+}
+function sound() {
+  selected="#soun";
+  iconsound.style.backgroundColor=selcolor;
+  iconback.style.backgroundColor=color;
+  iconcursor.style.backgroundColor=color;
+  iconhouse.style.backgroundColor=color;
+  iconmisc.style.backgroundColor=color;
+  main.hidden = true;
+  msg.textContent = "Sounds";
+  back.hidden = true;
+  cursors.hidden = true;
+  sounds.hidden = false;
+  misc.hidden= true;
+}
+function miscc() {
+  selected="#mis";
+  iconmisc.style.backgroundColor=selcolor;
+  iconback.style.backgroundColor=color;
+  iconcursor.style.backgroundColor=color;
+  iconsound.style.backgroundColor=color;
+  iconhouse.style.backgroundColor=color;
+  main.hidden = true;
+  msg.textContent = "Miscellaneous";
+  back.hidden = true;
+  cursors.hidden = true;
+  sounds.hidden = true;
+  misc.hidden= false;
+}
+home();
+
+
+//Light mode/darkmode
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function lightmode() {
+  color="#d6d7d7";
+  selcolor="#b5b9b9";
+  main.style.backgroundColor = "#d1d3d3";
+  document.getElementById("nav-bar").style.backgroundColor="#ededed";
+  msg.style.backgroundColor="#ededed";
+  msg.style.color="black";
+  document.getElementById("text").style.color="black";
+  m.style.backgroundColor="#d1d3d3";
+  $(".side").css("background-color", color);
+  $(selected).css("background-color", selcolor);
+  $(".mode").text("Light Mode");
+  $("#content").css("color", "black");
+}
+function darkmode(){
+  color="#292828";
+  selcolor="#4a4646";
+  main.style.backgroundColor = "#2e2c2c";
+  document.getElementById("nav-bar").style.backgroundColor="#121212";
+  msg.style.backgroundColor="#121212";
+  msg.style.color="white";
+  document.getElementById("text").style.color="white";
+  m.style.backgroundColor="#2e2c2c";
+  $(".side").css("background-color", color);
+  $(selected).css("background-color", selcolor);
+  $(".mode").text("Dark Mode");
+  $("#content").css("color", "white");
+}
+var box = document.getElementById("check")
+
+function check() {
+  lightmode();
+  document.cookie = "mode=light;";
+}
+function uncheck() {
+  darkmode();
+  document.cookie = "mode=dark;";
+}
+$("#check").change(function() {
+    if(this.checked) {
+        check();
+    } else {
+      uncheck();
+    }
 });
-app.get('/index.js', (req, res) => {
-  res.sendFile(process.cwd() + "/js/index.js");
-});
-app.get('*', (req, res) => {
-  res.sendFile(process.cwd() + "/html/404.html");
-})
-//Start server
-app.listen(port, (req, res) => {
-console.log(`server listening on port: ${port}`)
- });
+if (getCookie("mode")=="") {
+  uncheck();
+  document.cookie = "mode=dark;";
+  box.checked=false;
+}
+if (getCookie("mode") == "dark") {
+  uncheck();
+  box.checked=false;
+} else {
+  check();
+  box.checked=true;
+}
