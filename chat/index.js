12fe364cfe7696b416d324cb3f551d39d2d7bdec
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2023-02-02 21:35:01 -0500

    Create index.js

---
+++ chat/index.js
@@ -0,0 +1,86 @@
+var personlist = {}
+var name = ""
+var wss = new WebSocket("wss://chat-server.andrewsmiley.repl.co");
+document.querySelector("#msgbox").onkeydown = function(key) {
+  if(key.key == "Enter") {
+    wss.send(JSON.stringify({"action": "sendMessage", "message": document.querySelector("#msgbox").value, "sender": name}))
+    document.querySelector("#msgbox").value="";
+  }
+}
+wss.onopen = (event) => {
+  name = "bob"; //prompt("Name: ");
+  if(name.toLowerCase() == "rusa") {
+    name = "Rusha"
+  }
+  wss.send(JSON.stringify({"action": "sendName", "name": name}))
+}
+wss.onmessage = (event) => {
+  var data = JSON.parse(event.data);
+  console.log(data);
+  switch(data["sender"]) {
+    case "SYSTEM":
+      var ele = document.createElement("div")
+      var elem = document.querySelector("#messages");
+      ele.className = "message sysmessage"
+      ele.innerHTML = data["message"];
+      elem.appendChild(ele)
+      elem.scrollTop = elem.scrollHeight;
+      break;
+    case "MEMBERS":
+        Object.keys(personlist).forEach(i => {
+          removePerson(i);
+        });
+      console.log(data["message"])
+        data["message"].forEach(i => {
+          console.log(i)
+          addPerson(i);
+        });
+      break;
+    default:
+      var ele = document.createElement("div")
+      var elem = document.querySelector("#messages");
+      ele.className = "message"
+      data["message"].split(" ").forEach(i => {
+        if(i.includes("http://") || i.includes("https://")) {
+          if(i.includes(".png")||i.includes(".jpg")||i.includes(".jpeg")||i.includes(".gif")) {
+            ele.innerHTML = data["sender"] + ": "
+            var img = document.createElement("img");
+            img.style="width: 250px; padding-left: 10px;";
+            img.src = i;
+            img.alt = i;
+            elem.appendChild(ele)
+            elem.appendChild(img)
+          }
+        } else {
+          ele.innerHTML = data["sender"] + ": " + data["message"]
+          elem.appendChild(ele)
+        }
+      })
+      elem.scrollTop = elem.scrollHeight;
+      
+  }
+}
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+document.body.style.backgroundColor = choose(['#fc0303', '#fc7f03', '#fcf803', '#24fc03', '#03dffc', '#c203fc']);
+
+function evalName(name) {
+  if(Object.keys(personlist).includes(name)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function addPerson(name) {
+  var div = document.createElement("div")
+  div.innerHTML = name;
+  div.className = "person";
+  document.querySelector("#people").appendChild(div);
+  personlist[name] = div
+}
+function removePerson(name) {
+  personlist[name].remove();
+}
