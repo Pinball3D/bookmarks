0bba53a443b16c0a38fbc1c1b78944f4b475c436
Pinball3D <76449307+Pinball3D@users.noreply.github.com>
2023-02-08 07:26:06 -0500

    Update index.js

--- chat/adminTools/index.js
+++ chat/adminTools/index.js
@@ -1,7 +1,8 @@
 var wss = new WebSocket("wss://52.70.17.111.sslip.io/");
 const params = new URLSearchParams(window.location.search);
-const username = "andrew"; //parseInt(params.get("user"))
-const password = "minion2009"; //parseInt(params.get("user"))
+var up = prompt("Enter your username and password in the following format: username+password. ex: andrew+12345").split("+")
+const username = up[0]; // parseInt(params.get("user"))
+const password = up[1]; //parseInt(params.get("user"))
 function kick() {
   wss.send(
     JSON.stringify({
